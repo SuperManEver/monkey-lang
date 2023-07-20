d1462f9327e855debc2bd5feb374f3552239328e
Nik Luparev <nikita.luparev@gmail.com>
2023-07-20 22:41:35 +0500

    lexer adjustments

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -25,30 +25,32 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
 
-	// case '=':
-	// 	if l.peekChar() == '=' {
-	// 		ch := l.ch
-	// 		l.readChar()
-	// 		literal := string(ch) + string(l.ch)
-	// 		tok = token.Token{Type: token.EQ, Literal: literal}
-	// 	} else {
-	// 		tok = newToken(token.ASSIGN, l.ch)
-	// 	}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
+
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
-	// case '!':
-	// 	if l.peekChar() == '=' {
-	// 		ch := l.ch
-	// 		l.readChar()
-	// 		literal := string(ch) + string(l.ch)
-	// 		tok = token.Token{Type: token.NOT_EQ, Literal: literal}
-	// 	} else {
-	// 		tok = newToken(token.BANG, l.ch)
-	// 	}
+
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '*':
@@ -126,4 +128,12 @@ func (l *Lexer) readNumber() string {
 	}
 
 	return l.input[position:l.position]
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
