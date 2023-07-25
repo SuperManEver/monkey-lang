b7f91d32d2f682de757124ef4447b08819034d0c
Nik Luparev <nikita.luparev@gmail.com>
2023-07-25 15:57:36 +0500

    parser: first steps

--- parser/parser.go
+++ parser/parser.go
@@ -31,6 +31,10 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+
+	case token.RETURN:
+		return p.parseReturnStatement()
+
 	default:
 		return nil
 	}
@@ -106,4 +110,15 @@ func (p *Parser) ParseProgram() *ast.Program {
 	}
 
 	return program
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
 }
