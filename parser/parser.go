3f7c8ae75f6fe5102891a14e44814208fea93da4
Nik Luparev <nikita.luparev@gmail.com>
2023-07-28 17:38:58 +0500

    add spacing in code

--- parser/parser.go
+++ parser/parser.go
@@ -199,10 +199,12 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
+
 	if prefix == nil {
 		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
+
 	leftExp := prefix()
 
 	for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
