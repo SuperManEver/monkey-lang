bf53f2463c03f123e7b545304a1ab35627d9966a
Nik Luparev <nikita.luparev@gmail.com>
2023-09-06 10:26:59 +0500

    built in functions

--- object/object.go
+++ object/object.go
@@ -17,6 +17,7 @@ const (
 	NULL_OBJ     = "NULL"
 	FUNCTION_OBJ = "FUNCTION"
 	STRING_OBJ   = "STRING"
+	BUILTIN_OBJ  = "BUILTIN"
 
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
@@ -87,3 +88,12 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
